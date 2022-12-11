622744f22c07861a2cf0d20f4e429c6935582356
ff <94084187+FabricioMrtn@users.noreply.github.com>
2022-12-11 09:12:21 -0300

    alterations in dto class

--- src/main/java/com/nv/marketapi/dto/ProductDto.java
+++ src/main/java/com/nv/marketapi/dto/ProductDto.java
@@ -2,13 +2,19 @@ package com.nv.marketapi.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record ProductDto(
-		Long id,
+		@NotBlank
+		@Pattern(regexp = "\\d{8,14}")
 		Long code,
+		@NotBlank
 		String description,
+		@NotBlank
 		BigDecimal cost,
+		@NotBlank
 		BigDecimal sell,
 		String internalDescription
 		) {
-
 }
