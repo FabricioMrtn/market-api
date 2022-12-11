e3a2da61f37740c915891b0e97e24cab1dd5db58
ff <94084187+FabricioMrtn@users.noreply.github.com>
2022-12-11 06:56:52 -0300

    adicionando classe product

---
+++ src/main/java/com/nv/marketapi/model/Product.java
@@ -0,0 +1,12 @@
+package com.nv.marketapi.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+	private Long id;
+	private Long code;
+	private String description;
+	private BigDecimal costPrice;
+	private BigDecimal sellPrice;
+	
+}
