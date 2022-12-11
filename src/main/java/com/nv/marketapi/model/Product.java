2b1f5f12aab0f94763368dc1001f24cb3eedb3da
ff <94084187+FabricioMrtn@users.noreply.github.com>
2022-12-11 08:53:46 -0300

    atualizacoes na classe product

--- src/main/java/com/nv/marketapi/model/Product.java
+++ src/main/java/com/nv/marketapi/model/Product.java
@@ -2,11 +2,31 @@ package com.nv.marketapi.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity(name = "Product")
+@Table(name = "products")
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Long code;
 	private String description;
 	private BigDecimal costPrice;
 	private BigDecimal sellPrice;
+	private String internalDescription;
+
+	public Product(String description) {
+		this.description = stringLimiter(description, 50);
+	}
 	
+	public String stringLimiter(String text, int max) {
+		return text.length() <= max ? text : text.substring(0, max);
+	}
+
 }
